f4cc3186ddb9ac88ef3c4f14f7657bd050d66506
medPurple <medpurple.pro@gmail.com>
2024-08-01 17:48:58 +0200

    feat:test new git

--- frontend/src/view/notfound/notfound.js
+++ frontend/src/view/notfound/notfound.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './notfound.css';
 
-
+// test
 export default function NotFound() {
     return (
         <div>
-            <h1>Oops! You seem to be lost.</h1>
+            <h1>Oops! You seem to be lost. </h1>
         </div>
         )
     }
