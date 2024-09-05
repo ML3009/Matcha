4d37152dd46f719d16cdad31146327c8055f6eae
purple <medpurple.pro@gmail.com>
2024-09-05 13:42:40 +0200

    feat : working on infos page

---
+++ frontend/src/view/informations/index.jsx
@@ -0,0 +1,11 @@
+import InformationForm from '../../component/information_form/';
+
+function InformationPage() {
+    return (
+    <div>
+        <InformationForm/>
+    </div>
+    );
+}
+
+export default InformationPage;
